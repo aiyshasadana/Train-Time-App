a475e124a1b0b33ecd55d203fb58575ca33a79a5
Aiysha Sadana <aiyshasadana@gmail.com>
2019-09-16 13:40:19 -0400

    pushing changes to Train Time App

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+  // Set the configuration for your app
+  // TODO: Replace with your project's config object
+var config = {
+    apiKey: "AIzaSyBfbWl9PSa3n9Yhv6x0n79qDC7ZDtuj0ys",
+    authDomain: "train-time-app-f43a9.firebaseapp.com",
+    databaseURL: "https://train-time-app-f43a9.firebaseio.com",
+    storageBucket: "train-time-app-f43a9.appspot.com"
+  };
+  firebase.initializeApp(config);
+
+  // Get a reference to the database service
+  var database = firebase.database();
